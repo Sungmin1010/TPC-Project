aacc3cc84375aaa6087ea41aab8ceb05c0d0c42c
sungmin <lmsgsm1@gmail.com>
2021-02-03 00:03:26 +0900

    세번째 실습 : 리소스 다운로드

--- project02/src/kr/blue/Project02_B.java
+++ project02/src/kr/blue/Project02_B.java
@@ -30,6 +30,33 @@ public class Project02_B {
                 System.out.println(li.select(".info").first().text());
             }
 
+            //리소스 다운로드 받기
+            //mp3
+//            Element tag = doc.select("source").first();
+//            String dPath = tag.attr("src").trim();
+//            String fileName = dPath.substring(dPath.lastIndexOf("/")+1);
+
+            //img
+            Element img = doc.select(".img > img").first();
+            String dPath = "https://sum.su.or.kr:8888" + img.attr("src").trim();
+            String fileName = dPath.substring(dPath.lastIndexOf("/")+1);
+
+
+            Runnable r = new DownloadBroker(dPath, fileName);
+            Thread dLoad = new Thread(r);
+            dLoad.start();
+            for(int i=0; i<10; i++){
+                try{
+                    Thread.sleep(1000);
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
+                System.out.print(" "+(i+1));
+            }
+            System.out.println();
+            System.out.println("===================================");
+
+
         }catch(Exception e){
             e.printStackTrace();
         }
